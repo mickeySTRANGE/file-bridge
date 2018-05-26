6d98a4689fc2f843d19fc8829526b93a56cc11e6
mickeySTRANGE <spark2065871439@yahoo.co.jp>
2018-05-26 22:24:56 +0900

    upload_test

--- upload.php
+++ upload.php
@@ -1,5 +1,15 @@
 <?
 
+$uploaddir = '/app/files';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "Possible file upload attack!\n";
+}
+
 foreach(glob('*') as $file){
     if(is_file($file)){
         echo htmlspecialchars($file);
