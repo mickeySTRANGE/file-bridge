65f5920a613c81f99315aa170d131162f8b96eef
mickeySTRANGE <spark2065871439@yahoo.co.jp>
2018-05-26 22:10:08 +0900

    upload？

--- upload.php
+++ upload.php
@@ -1,5 +1,20 @@
 <?
 
+foreach(glob('*') as $file){
+    if(is_file($file)){
+        echo htmlspecialchars($file);
+        echo '<br>';
+    }
+}
+
+echo '<br>';
+
+foreach(glob('files/*') as $file){
+    if(is_file($file)){
+        echo htmlspecialchars($file);
+        echo '<br>';
+    }
+}
 
 
 ?>
