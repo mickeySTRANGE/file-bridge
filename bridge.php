6aa321485fbc21f93507c13a611e412c893750c7
mickeySTRANGE <spark2065871439@yahoo.co.jp>
2018-05-26 21:57:45 +0900

    枠

---
+++ bridge.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>upload</title>
+</head>
+
+<body>
+  <form enctype="multipart/form-data" action="upload.php" method="POST">
+    このファイルをアップロード: <input name="userfile" type="file" />
+    <input type="submit" value="ファイルを送信" />
+  </form>
+</body>
+
+</html>
