7cadc31b2e5d2e3886a60bc8105923851b354ac0
mickeySTRANGE <spark2065871439@yahoo.co.jp>
2018-08-30 12:50:05 +0900

    ini_set

--- upload.php
+++ upload.php
@@ -3,6 +3,13 @@
 $uploaddir = '/app/files';
 $uploadfile = $uploaddir . '/' . basename($_FILES['userfile']['name']);
 
+echo ini_get('upload_max_filesize');
+ini_set('upload_max_filesize', '2000M');
+echo ini_get('upload_max_filesize');
+
+ini_set('post_max_size', '2000M');
+ini_set('memory_limit', '3000M');
+
 echo '<pre>';
 var_dump($_FILES['userfile']['error']);
 echo '<br>';
