8f4b7883f7f19e2d3254285db465e7391ee9b143
mickeySTRANGE <spark2065871439@yahoo.co.jp>
2018-08-30 12:37:13 +0900

    disp_error

--- upload.php
+++ upload.php
@@ -4,6 +4,8 @@ $uploaddir = '/app/files';
 $uploadfile = $uploaddir . '/' . basename($_FILES['userfile']['name']);
 
 echo '<pre>';
+var_dump($_FILES['userfile']['error']);
+echo '<br>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     echo "File is valid, and was successfully uploaded.\n";
 } else {
